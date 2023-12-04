0500541922a54b991d5c4f373e8036ec01fe2623
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-12-04 23:08:24 +0100

    Initial commit, added home page, api route, dev experience, interactive try-out tool

---
+++ pages/api/get.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+  const { method } = req;
+  let { url } = req.query;
+
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  switch (method) {
+    case "GET":
+      try {
+        if (!url.includes("https://") || !url.includes("http://")) {
+          url = "https://" + url;
+        }
+
+        const response = await fetch(encodeURI(url));
+        const html = await response.text();
+        res.status(200).json({ html });
+      } catch (error) {
+        console.error("Error fetching HTML:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false, error: `Unhandled request method: ${method}` });
+      break;
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,86 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import hljs from "highlight.js/lib/common";
+import "highlight.js/styles/github.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const placeholder = "Enter website URL";
+const defaultUrl = "google.nl";
 
 export default function Home() {
+  const [url, setUrl] = useState(defaultUrl);
+  const [htmlContent, setHtmlContent] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [key, setKey] = useState(Date.now());
+
+  useEffect(() => {
+    hljs.highlightAll();
+  }, [htmlContent]);
+
+  const fetchHtml = async () => {
+    setLoading(true);
+    setKey(Date.now());
+    try {
+      const response = await fetch(`/api/get?url=${encodeURIComponent(url)}`);
+      const { html } = await response.json();
+      setHtmlContent(html);
+    } catch (error) {
+      console.error("Error fetching HTML:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between gap-8 p-24 text-neutral-900 dark:text-neutral-100 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
+      <div className="relative m-auto place-items-center after:absolute after:top-0 after:-z-20 after:h-[180px] after:w-[360px] after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+        <h1 className="inline-block text-6xl font-bold">EveryOrigin</h1>
+        <h2 className="text-4xl font-bold">The free CORS proxy</h2>
+      </div>
+
+      <div className="flex flex-col items-center gap-8">
+        <p>
+          EveryOrigin is a free CORS proxy that allows you to access any website from any origin. Inspired by
+          allorigins.win and whateverorigin.org which was a humble open source clone of AnyOrigin.com. All of them are
+          either died or dead slow now. 🩻 So I decided to make my own version. It's free and open source. No Api key
+          required. No rate limit. No annoying ads. No tracking. No bullshit. Just a simple CORS proxy. Enjoy!
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      </div>
+
+      <div className="font-sans">
+        <h2 className="mb-2 text-4xl font-bold">Usage</h2>
+        <div className="flex rounded p-2 text-neutral-900 dark:bg-neutral-100">
+          {"https://everyorigin.netlify.app/get?url="}
+          <div
+            contentEditable
+            className="flex-grow outline-none"
+            onInput={(e) => setUrl(e.currentTarget.textContent.replace(placeholder, ""))}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {defaultUrl}
+            {!url && <span className="pointer-events-none text-neutral-400">{placeholder}</span>}
+          </div>
+          <button
+            className="-m-2 ml-2 rounded-r bg-blue-600 px-4 font-bold text-neutral-50 transition-colors hover:bg-blue-800 active:bg-blue-500"
+            onClick={fetchHtml}
+          >
+            Fetch
+          </button>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        {loading && <p>Loading...</p>}
+        {htmlContent && (
+          <pre key={key}>
+            <code className="language-html max-w-[calc(100vw-4em)] overflow-hidden rounded bg-neutral-200 p-2 text-neutral-800">
+              {htmlContent}
+            </code>
+          </pre>
+        )}
       </div>
     </main>
-  )
+  );
 }
