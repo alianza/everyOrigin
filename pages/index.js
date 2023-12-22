e6c6b843f97f4e770defc78a0b50cf94f265e4e2
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-12-22 18:13:12 +0100

    Updates & fixes

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 import hljs from "highlight.js/lib/common";
 import "highlight.js/styles/github.css";
+import Loader from "@/components/loader";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const placeholder = "Enter website URL";
 const defaultUrl = "google.nl";
 const baseUrl = "everyorigin.jwvbremen.nl";
 
@@ -13,22 +13,34 @@ export default function Home() {
   const [url, setUrl] = useState(defaultUrl);
   const [htmlContent, setHtmlContent] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [key, setKey] = useState(Date.now());
 
   useEffect(() => {
+    if (!htmlContent) return;
     hljs.highlightAll();
-  }, [htmlContent]);
+  }, [htmlContent, loading]);
 
   const fetchHtml = async () => {
-    setLoading(true);
-    setKey(Date.now());
+    setError(null);
+    const start = Date.now();
     try {
-      const response = await fetch(`/api/get?url=${encodeURIComponent(url)}`);
+      if (!url) throw new Error("URL is required");
+      const validUrl = new URL(!url.includes("http://") && !url.includes("https://") ? `https://${url}` : url);
+      setLoading(true);
+      const response = await fetch(`/api/get?url=${encodeURIComponent(validUrl.toString())}`);
       const { html } = await response.json();
+      if (!html) throw new Error("No HTML content found");
+      if (html === htmlContent) return;
+      setKey(Date.now());
       setHtmlContent(html);
     } catch (error) {
       console.error("Error fetching HTML:", error);
+      setError(error);
     } finally {
+      const end = Date.now();
+      const duration = end - start;
+      if (duration < 1000) await new Promise((resolve) => setTimeout(resolve, 1000 - duration));
       setLoading(false);
     }
   };
@@ -37,9 +49,18 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between gap-8 p-24 text-neutral-900 dark:text-neutral-100 ${inter.className}`}
     >
-      <div className="relative m-auto place-items-center after:absolute after:top-0 after:-z-20 after:h-[180px] after:w-[360px] after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      <header className="absolute top-0 flex w-full justify-end p-2 ">
+        <p>
+          Authored by:&nbsp;
+          <a className="hover:underline" href="https://jwvbremen.nl" rel=" noopener noreferrer" target="_blank">
+            Jan-Willem van Bremen
+          </a>
+        </p>
+      </header>
+
+      <div className="relative m-auto place-items-center after:absolute after:top-0 after:-z-20 after:h-[180px] after:w-[360px] after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40">
         <h1 className="inline-block text-6xl font-bold">EveryOrigin</h1>
-        <h2 className="text-4xl font-bold">The free CORS proxy</h2>
+        <h2 className="text-3xl font-bold">The free CORS proxy</h2>
       </div>
 
       <div className="flex flex-col items-center gap-8">
@@ -52,17 +73,22 @@ export default function Home() {
       </div>
 
       <div className="font-sans">
-        <h2 className="mb-2 text-4xl font-bold">Usage</h2>
-        <div className="flex rounded p-2 text-neutral-900 dark:bg-neutral-100">
-          {`http://${baseUrl}/get?url=`}
-          <div
+        <h2 className="text-4xl font-bold">Usage</h2>
+        <h4 className="text-lg font-bold">Fill in the URL you want to fetch</h4>
+        <div className="flex rounded bg-neutral-300 p-2 text-neutral-900 dark:bg-neutral-100">
+          {`https://${baseUrl}/get?url=`}
+          <span
+            className={`flex-grow ${
+              url === defaultUrl ? "font-bold italic" : ""
+            } outline-none empty:before:cursor-text empty:before:text-neutral-400 empty:before:content-['Enter_website_URL']`}
             contentEditable
-            className="flex-grow outline-none"
-            onInput={(e) => setUrl(e.currentTarget.textContent.replace(placeholder, ""))}
+            suppressContentEditableWarning
+            onInput={(e) => setUrl(e.currentTarget.textContent)}
+            onKeyDown={(e) => e.key === "Enter" && fetchHtml()}
           >
             {defaultUrl}
-            {!url && <span className="pointer-events-none text-neutral-400">{placeholder}</span>}
-          </div>
+          </span>
+
           <button
             className="-m-2 ml-2 rounded-r bg-blue-600 px-4 font-bold text-neutral-50 transition-colors hover:bg-blue-800 active:bg-blue-500"
             onClick={fetchHtml}
@@ -72,10 +98,18 @@ export default function Home() {
         </div>
       </div>
 
-      <div>
-        {loading && <p>Loading...</p>}
-        {htmlContent && (
-          <pre key={key}>
+      <div className="flex flex-col items-center">
+        {loading && <Loader className="m-4" />}
+
+        {error && !loading && <p className="p-4">Error fetching HTML content: {error.message}</p>}
+        {htmlContent && !loading && (
+          <pre key={key} className="relative shadow-lg">
+            <button
+              className="absolute right-2 top-2 origin-center transition-transform hover:scale-110 active:scale-95"
+              onClick={() => setHtmlContent("")}
+            >
+              ✖️
+            </button>
             <code className="language-html max-w-[calc(100vw-4em)] overflow-hidden rounded bg-neutral-200 p-2 text-neutral-800">
               {htmlContent}
             </code>
